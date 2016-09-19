c9bba78329a94b130ba383ba297525863153b0fc
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-09-19 14:50:03 +0200

    First setup of the user interface.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+    @FXML
+    private TextField txtSonarScanner;
+    @FXML
+    private TextField txtSonarQubeServer;
+    @FXML
+    private Button btnBrowseSonarScanner;
+    @FXML
+    private Button btnGo;
+    @FXML
+    private ComboBox cmbGithubRepository;
+    @FXML
+    private ComboBox cmbGithubPullRequest;
+    @FXML
+    private PasswordField pwfGithubToken;
+    @FXML
+    private PasswordField pwfSonarQubeToken;
+    @FXML
+    private CheckBox chkGithubRemind;
+    @FXML
+    private CheckBox chkSonarQubeRemind;
+
+    @FXML
+    private void openFileDialog(ActionEvent event) {
+
+    }
 }
