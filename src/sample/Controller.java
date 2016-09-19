aac38809c08afc488a04eb9c2cae21068adba050
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2016-09-19 15:13:18 +0200

    Made a file dailog.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,18 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
     @FXML
     private TextField txtSonarScanner;
     @FXML
-    private TextField txtSonarQubeServer;
+    private TextField txtSonarQubeServerIP;
+    @FXML
+    private TextField txtSonarQubeServerPort;
     @FXML
     private Button btnBrowseSonarScanner;
     @FXML
@@ -28,6 +34,9 @@ public class Controller {
 
     @FXML
     private void openFileDialog(ActionEvent event) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        File sonarScanner = fileChooser.showOpenDialog(new Stage());
+        txtSonarScanner.setText(sonarScanner.getAbsolutePath());
     }
 }
